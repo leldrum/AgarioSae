9028efb58cf749269202c975602075faeddc4502
fortin233 <lukas.fortin@etu.unicaen.fr>
2025-03-25 15:04:12 +0100

    avancement

--- AgarioClientSae/src/main/java/com/example/agarioclientsae/EatPlayerAI.java
+++ AgarioClientSae/src/main/java/com/example/agarioclientsae/EatPlayerAI.java
@@ -3,33 +3,7 @@ package com.example.agarioclientsae;
 public class EatPlayerAI implements IStrategyAI{
 
     public double[] move(Enemy enemy){
-        //move player towards the mouse position
-
-        World world = World.getInstance();
-
-        enemy.setClosestEntityDistance(enemy.distanceTo(world.getPlayer().getPosition()));
-        enemy.setClosestEntity(world.getPlayer());
-
-        world.root.getChildren().forEach(entity ->{
-            switch (entity) {
-                case MoveableBody each:
-                    if (!each.equals(enemy)){
-                        if (enemy.distanceTo(each.getPosition()) < enemy.getClosestEntityDistance()) {
-                            enemy.setClosestEntityDistance(enemy.distanceTo(each.getPosition()));
-                            enemy.setClosestEntity(each);
-
-                        }
-                    }
-
-                default : break;
-            }
-
-        });
-
-
-
         return enemy.getClosestEntity().getPosition();
-
 
     }
 
--- AgarioClientSae/src/main/java/com/example/agarioclientsae/Enemy.java
+++ AgarioClientSae/src/main/java/com/example/agarioclientsae/Enemy.java
@@ -13,11 +13,16 @@ public class Enemy extends MoveableBody{
 
     private IStrategyAI strategy = new EatPlayerAI();
 
+    private World world = World.getInstance();
+
     Enemy(Group group, double initialSize){
         super(group, initialSize);
         //new Enemy made and added to the group
         entity.setCenterX((Math.random() * World.getMapLimitWidth() * 2) - World.getMapLimitWidth());
         entity.setCenterY((Math.random() * World.getMapLimitHeight() * 2) - World.getMapLimitHeight());
+
+        closestEntityDistance = (distanceTo(world.getPlayer().getPosition()));
+        closestEntity = (world.getPlayer());
 
         //puts the Enemy infront of all the food
         //puts bigger entities in front of smaller entities
@@ -27,11 +32,27 @@ public class Enemy extends MoveableBody{
     public void Update(){
         //move player towards the mouse position
 
+        //System.out.println(world.getEntities());
+
+        world.getEntities().forEach(entity -> {
+            switch (entity) {
+                case MoveableBody each:
+                    if (!each.equals(this)){
+                        if (distanceTo(each.getPosition()) < closestEntityDistance) {
+                            closestEntityDistance = (distanceTo(each.getPosition()));
+                            closestEntity = (each);
+
+                            System.out.println(closestEntity);
+
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        });
+
         moveToward(strategy.move(this));
-
-        
-
-        moveToward(closestEntity.getPosition());
 
         //check if player is colliding with anything
         checkCollision();
