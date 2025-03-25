a40ca3ad4d8b1b5def88eb6799dc5031729b9a38
fortin233 <lukas.fortin@etu.unicaen.fr>
2025-03-25 14:03:13 +0100

    commit avant merge

--- AgarioClientSae/src/main/java/com/example/agarioclientsae/EatPastilleAI.java
+++ AgarioClientSae/src/main/java/com/example/agarioclientsae/EatPastilleAI.java
@@ -5,8 +5,31 @@ public class EatPastilleAI implements IStrategyAI{
     @Override
     public double[] move(World world, Enemy enemy) {
 
-        return new double[0];
-    }
+            enemy.setClosestEntityDistance(enemy.distanceTo(world.getPlayers().get(0).getPosition()));
+            enemy.setClosestEntity(world.getPlayers().get(0));
+
+            world.root.getChildren().forEach(entity ->{
+                switch (entity) {
+                    case MoveableBody each:
+                        if (each.equals(enemy)){
+                            if (enemy.distanceTo(each.getPosition()) < enemy.getClosestEntityDistance()) {
+                                enemy.setClosestEntityDistance(enemy.distanceTo(each.getPosition()));
+                                enemy.setClosestEntity(each);
+
+                            }
+                        }
+
+                    default : break;
+                }
+
+            });
+
+
+
+            return enemy.getClosestEntity().getPosition();
+
+
+        }
 
     public void move(){
 
