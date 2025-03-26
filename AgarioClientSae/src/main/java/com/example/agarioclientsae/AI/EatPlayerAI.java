8a892c690e10b5f0304acb76c91b42066264c2b3
Bérot Mathis <mathis.berot@etu.unicaen.fr>
2025-03-26 08:59:01 +0100

    correction ia

--- AgarioClientSae/src/main/java/com/example/agarioclientsae/AI/EatPlayerAI.java
+++ AgarioClientSae/src/main/java/com/example/agarioclientsae/AI/EatPlayerAI.java
@@ -1,34 +1,63 @@
 package com.example.agarioclientsae.AI;
 
+import com.example.agarioclientsae.player.Player;
 import com.example.agarioclientsae.worldElements.Enemy;
 import com.example.agarioclientsae.worldElements.Entity;
 import com.example.agarioclientsae.worldElements.Food;
 import com.example.agarioclientsae.worldElements.World;
+import javafx.scene.Node;
 
 public class EatPlayerAI implements IStrategyAI {
 
     public double[] move(Enemy enemy){
-        System.out.println(World.getInstance().getEntities());
-        if(enemy.getClosestEntity() instanceof Food){
-            System.out.println(3);
+        World world = World.getInstance();
+        // Initialiser la distance minimale à une valeur élevée
+        double minDistance = Double.MAX_VALUE;
+        Enemy closestEnemy = null;
+        Player closestPlayer = null;
 
-            for (Entity entity : World.getInstance().getEntities()) {
-                if (!entity.equals(enemy) && !(entity instanceof Food)){
-                    System.out.println(4);
-                    enemy.setClosestEntity(entity);
-                    enemy.setClosestEntityDistance(enemy.distanceTo(entity.getPosition()));
-                    if (enemy.distanceTo(entity.getPosition()) < enemy.getClosestEntityDistance()) {
-                        enemy.setClosestEntityDistance(enemy.distanceTo(entity.getPosition()));
-                        enemy.setClosestEntity(entity);
-                        System.out.println(2);
-                        return enemy.getClosestEntity().getPosition();
-                    }
+        // Parcourir tous les nœuds de la scène pour trouver les ennemis
+        for (Node node : world.root.getChildren()) {
+            if (node instanceof Enemy) {
+                Enemy potentialTarget = (Enemy) node;
+                if (potentialTarget.equals(enemy)) {
+                    continue;
+                }
+                double distance = enemy.distanceTo(potentialTarget.getPosition());
+
+                // Vérifier si cet ennemi est plus proche que la précédente
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestEnemy = potentialTarget;
+                }
+            }else if(node instanceof Player){
+                Player potentialTarget = (Player) node;
+                if (potentialTarget.equals(enemy)) {
+                    continue;
+                }
+                double distance = enemy.distanceTo(potentialTarget.getPosition());
+
+                // Vérifier si cet ennemi est plus proche que la précédente
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestPlayer = potentialTarget;
                 }
             }
         }
 
-        System.out.println(1);
-        return enemy.getClosestEntity().getPosition();
+        // Si un ennemi a été trouvée, calculer le vecteur de déplacement vers elle
+        if (closestEnemy != null) {
+            double[] position = closestEnemy.getPosition();
+            return new double[]{position[0], position[1]};
+        }
+
+        if (closestPlayer != null) {
+            double[] position = closestPlayer.getPosition();
+            return new double[]{position[0], position[1]};
+        }
+
+        // Si aucun ennnemi n'est trouvée, rester immobile
+        return new double[]{0, 0};
 
 
     }
