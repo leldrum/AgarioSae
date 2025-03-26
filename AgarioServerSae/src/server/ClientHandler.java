4af2d7a5681b47a6994600ecd6a9a469a08cbfdf
Bérot Mathis <mathis.berot@etu.unicaen.fr>
2025-03-26 10:55:05 +0100

    wip

--- AgarioServerSae/src/server/ClientHandler.java
+++ AgarioServerSae/src/server/ClientHandler.java
@@ -1,19 +1,30 @@
 package server;
 
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
     private Socket socket;
     private GameServer server;
 
+    private PrintWriter out;
+
     public ClientHandler(Socket socket, GameServer server) {
         this.socket = socket;
         this.server = server;
+
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void run() {
-
+        server.addClient(out);
+        server.broadcastMessage("ID:69");
     }
 
 }
